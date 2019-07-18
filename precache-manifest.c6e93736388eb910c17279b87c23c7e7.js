87dd875be6905dbf96049676f8d7152d04236b1e
k.angkananon <kullapat@alefeducation.com>
2019-07-18 20:14:55 +0400

    Updates

---
+++ precache-manifest.c6e93736388eb910c17279b87c23c7e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "53a21751c76f01a3fda93e8464843543",
+    "url": "/kt-space/static/media/avatar-gym.53a21751.jpg"
+  },
+  {
+    "revision": "c7403b058d81d84b98c3",
+    "url": "/kt-space/static/js/runtime~main.c7403b05.js"
+  },
+  {
+    "revision": "f4e88127f4a80bb0f8d8",
+    "url": "/kt-space/static/js/main.f4e88127.chunk.js"
+  },
+  {
+    "revision": "a7c4fe4d4ca7444b055e",
+    "url": "/kt-space/static/js/1.a7c4fe4d.chunk.js"
+  },
+  {
+    "revision": "f4e88127f4a80bb0f8d8",
+    "url": "/kt-space/static/css/main.9a4882b3.chunk.css"
+  },
+  {
+    "revision": "25160044bc311508a923565942816a8f",
+    "url": "/kt-space/index.html"
+  }
+];
