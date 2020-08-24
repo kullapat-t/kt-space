e8b3de02403090a0a4393a7de06b029c867a2c65
kullapat.t <kullapat.t@hotmail.com>
2020-08-24 10:29:41 +0400

    Updates

---
+++ precache-manifest.69c9e058c8a580c1625f13fb707c3c0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e66789841885b13fe290dfea38dc9583",
+    "url": "/kt-space/index.html"
+  },
+  {
+    "revision": "f7900c1f7ee31d8e6081",
+    "url": "/kt-space/static/css/main.4043e92a.chunk.css"
+  },
+  {
+    "revision": "1851d42fd957a6e70b9d",
+    "url": "/kt-space/static/js/2.6f8e81de.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/kt-space/static/js/2.6f8e81de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7900c1f7ee31d8e6081",
+    "url": "/kt-space/static/js/main.fe4cec21.chunk.js"
+  },
+  {
+    "revision": "5b2ef6ba48a1f36f9515",
+    "url": "/kt-space/static/js/runtime-main.0de80baa.js"
+  },
+  {
+    "revision": "0b65f19e9871dbe9c02ec12ee7e78c24",
+    "url": "/kt-space/static/media/nitch-logo.0b65f19e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kt-space/precache-manifest.b605da150352524eb93db02c046d9c12.js"
+  "/kt-space/precache-manifest.69c9e058c8a580c1625f13fb707c3c0b.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/kt-space/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kt-space/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
