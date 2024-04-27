7c2309c583121dea543d8ea5d6cff93025e3e260
kullapat.t <kullapat.t@hotmail.com>
2024-04-27 10:09:04 +0700

    kt: update react to 18 (and other deps)

--- src/App.js
+++ src/App.js
@@ -1,50 +1,50 @@
-import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
+import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
 import {
   HashRouter as Router,
   Route,
-  Switch,
-  Redirect,
-} from 'react-router-dom';
-import { Home } from './components/home/Home';
-import { MainLayout } from './components/layout/MainLayout';
-import { Resume } from './components/resume/Resume';
-import About from './components/about/About';
+  Routes,
+  Navigate,
+} from 'react-router-dom'
+import { Home } from './components/home/Home'
+import { MainLayout } from './components/layout/MainLayout'
+import { Resume } from './components/resume/Resume'
+import About from './components/about/About'
 import { toast } from 'react-toastify'
 
 const App = () => {
-  const { isAuthenticated, error, user } = useAuth0();
+  const { isAuthenticated, error, user } = useAuth0()
 
   if (error) {
     toast.error(`Oops... ${error.message}`, { 
-      position: toast.POSITION.BOTTOM_CENTER, 
+      position: "bottom-center",
       hideProgressBar: true
     })
   }
   if (isAuthenticated) {
-    toast.success(`Login successfully!`, { 
-      position: toast.POSITION.BOTTOM_CENTER, 
+    toast.success(`Login successfully!`, {
+      position: "bottom-center",
       hideProgressBar: true,
       autoClose: 1500
     })
-    toast.info(`Hello, ${user.name}, New features will coming soon`, { 
-      position: toast.POSITION.BOTTOM_CENTER, 
+    toast.info(`Hello, ${user.name}, New features will coming soon`, {
+      position: "bottom-center",
       hideProgressBar: true
     })
   }
   return (
     <Router>
       <MainLayout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          {/* <Route exact path="/dev" component={Experiment} /> */}
-          <Route exact path="/about" component={About} />
-          <Route exact path="/resume" component={Resume} />
-          <Redirect from="*" to="/" />
-        </Switch>
+        <Routes>
+          <Route exact path="/" element={ <Home/> } />
+          {/* <Route exact path="/dev" component={ <Experiment/> } /> */}
+          <Route exact path="/about" element={ <About/> } />
+          <Route exact path="/resume" element={ <Resume/> } />
+          <Route path='*' element={<Navigate replace={true} to="/" />} />
+        </Routes>
       </MainLayout>
     </Router>
-  );
-};
+  )
+}
 
-export default App;
+export default App
